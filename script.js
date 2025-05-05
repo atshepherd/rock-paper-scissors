b140376d911ea7d8ec48ae47ae32cd493493bfef
alex <23156287+atshepherd@users.noreply.github.com>
2025-05-05 16:34:56 -0400

    Define var, add switch statement to return choice

--- script.js
+++ script.js
@@ -2,8 +2,20 @@
 function getComputerChoice() {
 
 // Return a random value of rock, paper, or scissors (1, 2, or 3)
-    return Math.floor((Math.random() * 3) + 1);
-    
+let randNum = Math.floor((Math.random() * 3) + 1);
+
+    switch(randNum){
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors";
+            break;
+    }
+
 }
 
 console.log(getComputerChoice());
