790f3b602d77868a9e0794c30f884f064123d19e
alex <23156287+atshepherd@users.noreply.github.com>
2025-05-05 16:20:51 -0400

    Add random generation and test function

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Create a new function named getComputerChoice
 function getComputerChoice() {
 
-// Return a random value of rock, paper, or scissors
+// Return a random value of rock, paper, or scissors (1, 2, or 3)
+    return Math.floor((Math.random() * 3) + 1);
     
-
 }
 
+console.log(getComputerChoice());
