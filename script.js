2c7b1dc508afcd5e8c7b149f637243a4be79ce7b
alex <23156287+atshepherd@users.noreply.github.com>
2025-05-05 22:48:27 -0400

    Remove debug

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 function getComputerChoice() {
 
 // Return a random value of rock, paper, or scissors (1, 2, or 3)
-let randNum = Math.floor((Math.random() * 3) + 1);
+let computerChoice = Math.floor((Math.random() * 3) + 1);
 
-    switch(randNum){
+    switch(computerChoice){
         case 1:
             return "Rock";
             break;
@@ -18,15 +18,16 @@ let randNum = Math.floor((Math.random() * 3) + 1);
 
 }
 
+// 
 // console.log(getComputerChoice()); for debug
 
 // Create a new function named getHumanChoice
 function getHumanChoice() {
-    let choice = prompt("Please enter your choice of Rock, Paper, or Scissors.");
-    return choice;
+    // Take user input, assume correct input
+    let humanChoice = prompt("Please enter your choice of Rock, Paper, or Scissors.");
+    return humanChoice;
 }
 
-// Take user input, assume correct input
 // console.log(getHumanChoice()); debug
 
 // Create two variables named humanScore and computerScore
