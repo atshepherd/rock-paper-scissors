62b1ce8d49f8de46e3c408553b89da4efe8a6ffc
alex <23156287+atshepherd@users.noreply.github.com>
2025-05-07 16:11:40 -0400

    Add logic to increment score

--- script.js
+++ script.js
@@ -46,30 +46,34 @@ if ( (humanChoice === "rock" && computerChoice == "rock") || (humanChoice === "p
 //If player chooses rock
 else if (humanChoice === "rock" && computerChoice == "paper") {
     console.log("You lose. Paper beats rock.");
+    computerScore++;
 }
 else if (humanChoice === "rock" && computerChoice == "scissors") {
     console.log("You win! Rock beats scissors!");
+    humanScore++;
 }
 
 //If player chooses paper
 else if (humanChoice === "paper" && computerChoice == "rock") {
     console.log("You win! Paper beats rock.");
+    humanScore++;
 }
 else if (humanChoice === "paper" && computerChoice == "scissors") {
     console.log("You lose. Scissors beats paper.");
+    computerScore++;
 }
 
 //If player chooses scissors
 else if (humanChoice === "scissors" && computerChoice == "paper") {
     console.log("You win! Scissors beats paper.");
+    humanScore++;
 }
 else if (humanChoice === "scissors" && computerChoice == "rock") {
     console.log("You lose. Rock beats scissors");
+    computerScore++;
 }
-
-// Increment score based on winner
 
 }
 
 playRound(getHumanChoice(), getComputerChoice());
-
+console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
