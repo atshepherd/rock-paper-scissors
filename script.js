6648cc75b64c0bcbc05216adb8a12b8ddbe32ef3
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-06-09 16:47:28 -0400

    Refactor playRound to track score

--- script.js
+++ script.js
@@ -29,92 +29,82 @@ function getHumanChoice() {
 // Create a function named playRound, takes parameters humanChoice and computerChoice
 function playRound(humanChoice, computerChoice) {
 
-// Make humanChoice case-insensitive
-    humanChoice.toLowerCase();
-    computerChoice.toLowerCase();
+// Create two variables named humanScore and computerScore
+// Initialize these variables with the value of 0
+let humanScore = 0;
+let computerScore = 0;
+
+// Create a variable to store the result from playRound, initialize to empty string
+let result = "";
 
 // Tie condition
 if ( (humanChoice === "rock" && computerChoice == "rock") || (humanChoice === "paper" && computerChoice == "paper") || (humanChoice === "scissors" && computerChoice == "scissors")) {
     console.log("It's a tie!");
-    return "tie";
     // Log the result and return the result for scorekeeping
 }
 
 //If player chooses rock
 else if (humanChoice === "rock" && computerChoice == "scissors") {
     console.log("You win! Rock beats scissors!");
-    return "win";
+    humanScore++;
     // Log the result and return the result for scorekeeping
 
 }
 else if (humanChoice === "rock" && computerChoice == "paper") {
     console.log("You lose. Paper beats rock.");
-    return "lose";
+    computerScore++;
     // Log the result and return the result for scorekeeping
 }
 
 //If player chooses paper
 else if (humanChoice === "paper" && computerChoice == "rock") {
     console.log("You win! Paper beats rock.");
-    return "win";
+    humanScore++;
     // Log the result and return the result for scorekeeping
 }
 else if (humanChoice === "paper" && computerChoice == "scissors") {
     console.log("You lose. Scissors beats paper.");
-    return "lose";
+    computerScore++;
     // Log the result and return the result for scorekeeping
 }
 
 //If player chooses scissors
 else if (humanChoice === "scissors" && computerChoice == "paper") {
     console.log("You win! Scissors beats paper.");
-    return "win";
+    humanScore++;
     // Log the result and return the result for scorekeeping
 }
 else if (humanChoice === "scissors" && computerChoice == "rock") {
     console.log("You lose. Rock beats scissors");
-    return "lose";
+    computerScore++;
     // Log the result and return the result for scorekeeping
 }
+
+    // Increment score based on result from playRound
+    switch(result) {
+    case "win":
+        break;
+    case "lose":
+        break;
+    case "tie":
+        break;      
+    }
+
+    // Announce a winner and output the score of both players
+    if (humanScore > computerScore) {
+    console.log(`You win! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
+    }
+    else if (computerScore > humanScore) {
+    console.log(`You lose. Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
+    }
+    else if (humanScore === computerScore) {
+    console.log(`It's a tie! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
+    }
 
 }
 
 // Create a new function named playGame
 function playGame() {
-
-    // Create two variables named humanScore and computerScore
-    // Initialize these variables with the value of 0
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // Create a variable to store the result from playRound, initialize to empty string
-    let result = "";
-
-    // Play the round, get both choices, return result
-    result = playRound(getHumanChoice(), getComputerChoice());
-
-    // Increment score based on result from playRound
-    switch(result) {
-        case "win":
-            humanScore++;
-            break;
-        case "lose":
-            computerScore++;
-            break;
-        case "tie":
-            break;      
-    }
-
-    // Announce a winner and output the score of both players
-    if (humanScore > computerScore) {
-        console.log(`You win! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
-    }
-    else if (computerScore > humanScore) {
-        console.log(`You lose. Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
-    }
-    else if (humanScore === computerScore) {
-        console.log(`It's a tie! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
-    }
 }
 
 let rockBtn = document.getElementById("rock");
