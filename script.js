0f6dcb44ff34a400736e29be08403bfbfac29877
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-06-10 12:58:41 -0400

    Fix scorekeeping

--- script.js
+++ script.js
@@ -19,14 +19,27 @@ let computerChoice = Math.floor((Math.random() * 3) + 1);
 
 }
 
+function keepScore(humanScore, computerScore) {
+    // Announce a winner and output the score of both players
+    if (humanScore == 5 || computerScore == 5) {
+        announceWinner(humanScore, computerScore);
+    }
+}
+
+function announceWinner(humanScore, computerScore) {
+    if (humanScore > computerScore) {
+    console.log(`You win! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
+    }
+    else if (computerScore > humanScore) {
+    console.log(`You lose. Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
+    }
+    else if (humanScore === computerScore) {
+    console.log(`It's a tie! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
+    }
+}
+
 // Create a function named playRound, takes parameters humanChoice and computerChoice
 function playRound(humanChoice, computerChoice) {
-
-// Create two variables named humanScore and computerScore
-// Initialize these variables with the value of 0
-let humanScore = 0;
-let computerScore = 0;
-
 // Tie condition
 if ( (humanChoice === "rock" && computerChoice == "rock") || (humanChoice === "paper" && computerChoice == "paper") || (humanChoice === "scissors" && computerChoice == "scissors")) {
     console.log("It's a tie!");
@@ -63,37 +76,34 @@ else if (humanChoice === "scissors" && computerChoice == "rock") {
     computerScore++;
 }
 
-// Announce a winner and output the score of both players
-if (humanScore > computerScore) {
-console.log(`You win! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
-}
-else if (computerScore > humanScore) {
-console.log(`You lose. Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
-}
-else if (humanScore === computerScore) {
-console.log(`It's a tie! Your score was ${humanScore}, and the computer's score was ${computerScore}.`);
-}
-
 }
 
 let rockBtn = document.getElementById("rock");
 let paperBtn = document.getElementById("paper");
 let scissorsBtn = document.getElementById("scissors");
 
+// Create two variables named humanScore and computerScore
+// Initialize these variables with the value of 0
+let humanScore = 0;
+let computerScore = 0;
+
 rockBtn.addEventListener("click", () => {
     let computerSelection = getComputerChoice();
     let playerSelection = "rock";
     playRound(playerSelection, computerSelection);
+    keepScore(humanScore, computerScore);
 });
 
 paperBtn.addEventListener("click", () => {
     let computerSelection = getComputerChoice();
     let playerSelection = "paper";
     playRound(playerSelection, computerSelection);
+    keepScore(humanScore, computerScore);
 });
 
 scissorsBtn.addEventListener("click", () => {
     let computerSelection = getComputerChoice();
     let playerSelection = "scissors";
     playRound(playerSelection, computerSelection);
+    keepScore(humanScore, computerScore);
 });
